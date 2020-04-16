1821d19eca9a4d1d7133b07a22f616ccdb7a6473
pontu <b19ponka@student.his.se>
2020-04-16 15:32:21 +0200

    Create ArrayList Create ArrayAdapter Reference to ListView Connect ArrayAdapter to ListView Add item click listener

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+    //private String[] mountainLocations = {"Alps","Alps","Alaska"};
+    //private int[] mountainHeight = {4478,4808,6190};
+    private ArrayList<String> listData=new ArrayList<>(Arrays.asList(mountainNames));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.list_item_textview,R.id.list_item_textview,listData);
+        ListView listView=(ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
